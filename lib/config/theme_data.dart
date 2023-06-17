180d65cfd3f703fd5322ec7d16fd61c361d45bf2
AbdullahFathan <abdullahfathan013@gmail.com>
2023-06-17 16:32:40 +0700

    style: add some style at home page

--- lib/config/theme_data.dart
+++ lib/config/theme_data.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
 const smokeWhiteColor = Color(0xfff3f3f3);
+const raisinBlackColor = Color(0xff262729);
 
 ThemeData themeData = ThemeData(
   scaffoldBackgroundColor: smokeWhiteColor,
   fontFamily: 'Nunito',
   textTheme: const TextTheme(
-      titleLarge: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-      ),
-      titleMedium: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.w600,
-      )),
+    titleLarge: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 18,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
 );
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fe_store/config/theme_data.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +9,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String dropdownValue = 'Electronics';
+  List<String> categoryList = [
+    'Electronics',
+    'Jewelery',
+    'Men`s clothing',
+    'Women`s clothing'
+  ];
+
+  String limitValue = '1';
+  List<String> limitList = ['1', '2', '3', '4', '5'];
   @override
   Widget build(BuildContext context) {
+    double witdhContainer = MediaQuery.of(context).size.width;
+
     return SafeArea(
       child: SingleChildScrollView(
           child: Padding(
@@ -18,13 +31,36 @@ class _HomePageState extends State<HomePage> {
           vertical: 20,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(
+              'Selamat Datang',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 30, bottom: 20),
+              child: Text(
+                "Find some item ??",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  "Selamat Datang",
-                  style: Theme.of(context).textTheme.titleLarge,
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search),
+                  label: Text(
+                    'Search Some Item',
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(witdhContainer * 0.7, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    backgroundColor: raisinBlackColor,
+                  ),
                 ),
                 Material(
                   type: MaterialType.transparency,
@@ -47,9 +83,98 @@ class _HomePageState extends State<HomePage> {
                 )
               ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
+            //{FIlter Items}
+            Container(
+              width: witdhContainer,
+              height: 90,
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 18),
+              padding: const EdgeInsets.only(top: 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          'Category',
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(width: 1, color: Colors.black)),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<String>(
+                            padding: const EdgeInsets.only(left: 5),
+                            value: dropdownValue,
+                            items: categoryList
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  //Limit
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4),
+                        child: Text(
+                          'Limit',
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ),
+                      Container(
+                        width: 90,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(width: 1, color: Colors.black)),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton<String>(
+                            padding: const EdgeInsets.only(left: 5),
+                            value: limitValue,
+                            items: limitList
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                limitValue = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       )),
