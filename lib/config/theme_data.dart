f83081d365088595c9049458779b18fb5d33f7c9
AbdullahFathan <abdullahfathan013@gmail.com>
2023-06-18 18:57:39 +0700

    style: cart ui done

--- lib/config/theme_data.dart
+++ lib/config/theme_data.dart
@@ -8,30 +8,35 @@ ThemeData themeData = ThemeData(
   scaffoldBackgroundColor: smokeWhiteColor,
   fontFamily: 'Nunito',
   textTheme: const TextTheme(
-      titleLarge: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-      ),
-      titleMedium: TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.w600,
-      ),
-      labelMedium: TextStyle(
-        fontSize: 18,
-        color: Colors.white,
-        fontWeight: FontWeight.w600,
-      ),
-      labelSmall: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w700,
-      ),
-      bodySmall: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w600,
-        color: brownColor,
-      ),
-      bodyMedium: TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w700,
-      )),
+    titleLarge: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 18,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: brownColor,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
 );
--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,18 +1,69 @@
+import 'package:fe_store/screens/widgets/detail_card.dart';
 import 'package:flutter/material.dart';
 
-class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+import '../data/models/product_model.dart';
 
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
 
-class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Center(
-        child: Text("cart page"),
+    double witdhContainer = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            flexibleSpace: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: Text(
+                      'Your Cart',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return DetailCard(
+                  witdhContainer: witdhContainer,
+                  productModel: dummyData, // Replace with your actual data
+                );
+              },
+              childCount:
+                  6, // Replace with the actual number of items in your cart
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 16),
+              padding: const EdgeInsets.all(16.0),
+              color: Colors.grey[200],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total Price:',
+                      style: Theme.of(context).textTheme.bodyLarge),
+                  Text('\$100', // Replace with your dynamic total price
+                      style: Theme.of(context).textTheme.bodyLarge),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
